ae12e00769516886b306e3d0921a61af7db8d797
tawhid <ah5229530@gmail.com>
2025-03-13 21:51:35 +1100

    Update Trees.py

--- Trees.py
+++ Trees.py
@@ -1,53 +1,55 @@
+
 class Node:
     def __init__(self, value):
-        self.value=value
+        self.value= value
         self.right=None
         self.left=None
 
 class BinarySearchTree:
+    
     def __init__(self):
         self.root=None
 
     def insert(self, value):
-        new_node=Node(value)
+        new_node= Node(value)
         if self.root is None:
             self.root=new_node
-            return True
         temp=self.root
-        while (True):
-            if new_node.value==temp.value:
+        while True:
+            if temp.value==new_node.value:
                 return False
             if new_node.value>temp.value:
                 if temp.right is None:
                     temp.right=new_node
                     return True
-                temp=temp.right
+                temp= temp.right
             if new_node.value<temp.value:
                 if temp.left is None:
                     temp.left=new_node
                     return True
-                temp=temp.left
+                temp= temp.left
 
-    def contains(self,value):
+    
+    def contains(self, value):
         temp=self.root
         while temp is not None:
-            if temp.value==value:
-                print("found")
+            if value==temp.value:
+                print(" found")
                 return True
             elif value>temp.value:
-                temp=temp.right
+                temp= temp.right
             elif value<temp.value:
-                temp=temp.left
+                temp= temp.left
 
         print("not found")
         return False
-
-                
-
-        
         
 
+        
 
+    
+    
+    
 my_binary_tree=BinarySearchTree()
 my_binary_tree.insert(4)
 my_binary_tree.insert(5)
@@ -57,4 +59,41 @@ my_binary_tree.insert(3)
 
 # print(my_binary_tree.root.value)
 # print(my_binary_tree.root.left.value)
-my_binary_tree.contains(1)
+my_binary_tree.contains(3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
