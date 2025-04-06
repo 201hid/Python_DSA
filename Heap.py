acf3dc58796e97898bd9d7d963faf8d58fa0ce7f
tawhid <ah5229530@gmail.com>
2025-04-06 22:11:11 +1000

    Update Heap.py

--- Heap.py
+++ Heap.py
@@ -22,6 +22,42 @@ class MaxHeap:
             self.swap(self.heap[current], self.heap[self._parent(current)])
             current=self._parent(current)
 
+    def sink_down(self,index):
+        current= self.heap[0]
+        max_index=index
+        while True:
+            right_child=self._right_child(index)
+            left_child=self._left_child(index)
+
+            if self.heap[right_child]>self.heap[max_index]:
+                max_index=right_child
+            if self.heap[left_child]>self.heap[max_index]:
+                max_index=left_child
+
+
+
+
+    def remove(self):
+        if len(self.heap) == 0:
+            return None
+        if self.heap ==1:
+            self.heap.pop()
+        
+        max_value=self.heap[0]
+        self.heap[0]= self.heap.pop()
+        self.sink_down(0)
+        return max_value
+    
+
+        
+
+
+            
+
+
+        
+
+
 myheap= MaxHeap()
 myheap.insert(99)
 myheap.insert(72)
